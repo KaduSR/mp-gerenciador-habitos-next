76971c704730a1c203b849127a6d4ad1b46e941d
venblaras <carlos847@gmail.com>
2025-06-16 10:12:14 -0300

    feat: corrige a exibição do nome do hábito na lista de hábitos

--- meta-diaria-habits/src/app/components/HabitsList.js
+++ meta-diaria-habits/src/app/components/HabitsList.js
@@ -1,13 +1,18 @@
 import styles from "@/styles/HabitsList.module.css";
 import HabitCard from "./HabitCard";
 
+
+
 export default function HabitsList({ habits = [] }) {
   return (
     <section className={styles.list}>
       {habits.length === 0 ? (
         <p className={styles.empty}>Você ainda não tem hábitos cadastrados. </p>
       ) : (
-        habits.map((habit) => <HabitCard key={habit.id} habit={habit} />)
+          habits.map((habit) => (<HabitCard key={habit.id} habit={{ id: habit.id, name: hbit.name }}
+            initialDone={habit.done}
+          />
+          ))
       )}
     </section>
   );
